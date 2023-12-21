9857be0aded38a486c9c79edb7ebcc2b741d13d0
netxpertsaws <98447003+netxpertsaws@users.noreply.github.com>
2023-12-21 09:30:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,5 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA4WG2YRJRY3DD5PMU"
-  secret_key = "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"
+  region     = "us-east-1"  
 }
